4aa3a174426774a7093158c298164047dbe78a47
Diego Cardoso <diegopc86@gmail.com>
2017-02-10 19:26:37 -0200

    Add Firebase initialization

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
+
 import App from './App';
-import './index.css';
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+});
 
 ReactDOM.render(
   <App />,
