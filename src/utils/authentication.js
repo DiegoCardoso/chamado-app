2c20d8d9cce23a68091297b30b01d5ce18ce72ae
Diego Cardoso <diegopc86@gmail.com>
2017-02-12 21:49:55 -0200

    Change methods at authentication

--- src/utils/authentication.js
+++ src/utils/authentication.js
@@ -1,7 +1,11 @@
 import firebase from 'firebase';
 
-export const authenticate = (email, password) => firebase.auth().signInWithEmailAndPassword(email, password);
+export const authenticate = (email, password) => (
+  firebase.auth().signInWithEmailAndPassword(email, password)
+);
 
 export const loggedUser = () => firebase.auth().currentUser;
 
-export const isLogged = () => loggedUser() !== null;
+export const isLoggedIn = () => (loggedUser() !== null);
+
+export const loggout = () => firebase.auth().signOut();
