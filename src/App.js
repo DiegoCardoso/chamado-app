71b601ec36e5a74ff4ba4e236d669e37e0caee62
Diego Cardoso <diegopc86@gmail.com>
2017-02-12 21:57:26 -0200

    Add <Dashboard /> container to <App />

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import {
-  BrowserRouter as Router
+  BrowserRouter as Router,
+  Route
 } from 'react-router-dom';
 
-import Login from './container/Login'
+import { injectGlobal } from 'styled-components'
+
+import Login from './container/Login';
+import Dashboard from './container/Dashboard';
 
 class App extends Component {
   render() {
+    (() => injectGlobal`
+      body {
+        padding: 0;
+        margin: 0;
+      }
+    `)()
     return (
       <Router>
-        <Login />
+        <div>
+          <Route path="/" component={Login} />
+          <Dashboard />
+        </div>
       </Router>
     );
   }
 }
+
 
 export default App;
