b85dd8ea4634a745cad505b638f586799231aa94
Diego Cardoso <diegopc86@gmail.com>
2017-02-12 21:55:17 -0200

    Change <Login /> container for better state managment

--- src/container/Login/index.js
+++ src/container/Login/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 import LoginComponent from '../../components/Login';
-import { authenticate } from '../../utils/authentication';
+import Loading from '../../components/Loading';
+import { authenticate, isLoggedIn } from '../../utils/authentication';
 
 class Login extends React.Component {
 
+  constructor() {
+    super();
+    setTimeout(() => {
+      this.setState({
+        isLoggedIn: isLoggedIn(),
+        loading: false
+      });
+    }, 300)
+  }
+
   state = {
+    loading: true,
+    isLoggedIn: false,
     errorMessage: ''
   }
 
@@ -21,21 +35,37 @@ class Login extends React.Component {
       });
     }
 
-    authenticate(email, password).catch(({ code, message }) => {
-      if (code === 'auth/invalid-email') {
-        return this.setState({
-          errorMessage: 'Formato em e-mail inválido'
-        })
-      }
-
-      if (code === 'auth/wrong-password') {
+    authenticate(email, password)
+      .then(() => {
         this.setState({
-          errorMessage: 'E-mail/senha inválidos'
+          isLoggedIn: true
         })
-      }
-    });
+      })
+      .catch(({ code, message }) => {
+        if (code === 'auth/invalid-email') {
+          return this.setState({
+            errorMessage: 'Formato em e-mail inválido'
+          })
+        }
+        if (code === 'auth/wrong-password') {
+          this.setState({
+            errorMessage: 'E-mail/senha inválidos'
+          })
+        }
+      });
   }
   render() {
+
+    if (this.state.loading) {
+      return <Loading />
+    }
+
+    const { from } = this.props.location.state || '/dashboard';
+    if (this.state.isLoggedIn) {
+      return (
+        <Redirect to={from} />
+      );
+    }
     return (
       <LoginComponent
         onLoginSubmit={this.onLoginSubmit}
