0ae85f4854b4b12621cdd33b8116f1fe8c1f7339
Diego Cardoso <diegopc86@gmail.com>
2017-02-12 22:47:52 -0200

    Create <Dashboard /> component

---
+++ src/components/Dashboard/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Container from '../Container';
+import AppHeader from '../AppHeader';
+
+const Dashboard = () => (
+  <Container>
+    <AppHeader />
+    <h1>
+      Dashboard
+    </h1>
+  </Container>
+);
+
+export default Dashboard;
