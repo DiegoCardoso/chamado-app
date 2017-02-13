ea0085cc802c9faee93f055fa8be0aa629c627c6
Diego Cardoso <diegopc86@gmail.com>
2017-02-12 22:47:28 -0200

    Add <AppHeader /> component

---
+++ src/components/AppHeader/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { grey700 } from '../../utils/colors';
+import logo from '../../images/logo.svg';
+
+const Header = styled.header`
+  background-color: #FFF;
+  border-bottom: 2px solid ${ grey700};
+  padding: 0.5em;
+  margin: -10px;
+  margin-bottom: 10px;
+`;
+
+export default () => (
+  <Header>
+    <img src={logo} width="200px" />
+  </Header>
+)
